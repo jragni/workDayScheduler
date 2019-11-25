3b13ce50fffaca60a4f73c1cd889591e093471a0
Jhensen Agni <jagni001@ucr.edu>
2019-11-24 23:28:28 -0800

    added aesthetics and save functionaility. version should be fully capable of performing tasks

--- script.js
+++ script.js
@@ -5,8 +5,9 @@
 
 
 var _times = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+var _militaryTime = [ 9,10,11,12,13,14,15,16,17];
 var save = $(".save");
-
+var currentHour = moment().hour();
 var schedule = [];
 //create an object that stores plan and time
 //check if any schedule is stored in localStorage
@@ -16,7 +17,7 @@ var schedule = [];
 $(document).ready(function()
     {
 
-
+        // check if there is schedule in LOCALSTORAGE
         if(localStorage.getItem("schedule")!=null)
         {
             schedule = JSON.parse(localStorage.getItem("schedule"));
@@ -27,11 +28,12 @@ $(document).ready(function()
                 {
                     schedule[j]= {
                                     time: _times[j],
-                                    plan: ""       
+                                    plan: "",
+                                    milTime: _militaryTime[j]     
                                 };
                 }
         }
-    // for all the times after 9 
+        // for all the times after 9 
         // create a div container with attributes class: schedule-hour row AND attribute id: times[i]
         // create a div with the time 
         // create a text area with attribute class: col-sm-6 scheduleplan rows=3 cols=100
@@ -40,8 +42,8 @@ $(document).ready(function()
         for(var i = 0; i < _times.length; i++ )
             {
                 var scheduleHour = $("<div>").attr({class:"schedule-hour row", id:_times[i]});
-
-                var timeOfDay = $("<div>").attr({class:"col-sm-3 time-of-day"}).html(_times[i]);
+              
+                var timeOfDay = $("<div>").attr({class:"col-sm-3 time-of-day",id:"t-o-d-"+_times[i]}).html(_times[i]);
 
                 var textbox = $("<textarea>").attr({class:"col-sm-6 schedule-plan rows='3' cols='100' ",id:"text-"+_times[i]});
                 // if the text box does have a previously stored value, 
@@ -60,14 +62,46 @@ $(document).ready(function()
                 save.appendTo(scheduleHour);
                 scheduleHour.appendTo("#schedule");
             }
+
+
+        var clock = setInterval(function()
+            {
+                var Time = $("#current-time");
+                if (moment().hour() > 12)
+                {   
+                    Time.html( (moment().hour()-12) + ":" + moment().minute() + ":" + moment().second() + " PM");
+                    if (moment().minute()<10)
+                    {
+                        // add a 0 to the time if less than 10
+                        Time.html( (moment().hour()-12) + ":" + "0" +moment().minute() + ":" + moment().second() + " PM");
+                    }
+
+
+                } 
+                else
+                {
+                Time.html(moment().hour() + ":" + moment().minute() + ":" + moment().second() + " AM");
+                if (moment().minute()<10)
+                {
+                    // add a 0 to the time if less than 10
+                    Time.html(moment().hour() + ":" + "0" +moment().minute() + ":" + moment().second() + " AM");
+                }
+                }
+
+                UpdatePlannerTimeBlocks();
+                
+
+            },10);
+
+
+        
     });
   
 
 // on click of the save box
-    // the textbox corresponding to the time will be pushed to an object with t
 
-
-document.addEventListener("click",function(event){
+document.addEventListener("click",function(event)
+{
     event.preventDefault();
    
     event.stopPropagation();
@@ -78,11 +112,35 @@ document.addEventListener("click",function(event){
                    var Plan = $("#text-"+_times[i]).val();  // Get the value of the string that correlates to the save
                    // store the plan in the schedule
                     schedule[i].plan = Plan;
+                    alert("Saved")
                 }
         }
         
         localStorage.setItem("schedule",JSON.stringify(schedule));
+        
 });
 
+function UpdatePlannerTimeBlocks()
+        {
+            for(var workTime = 9; workTime < 17 ; workTime++)
+                {
+                    for( var jj = 0; jj< _times.length; jj++ )
+                        {
+                            
+                            if( currentHour > schedule[jj].milTime)
+                                {
+                                    document.getElementById(_times[jj]).style.background = "#64485C";
 
-
+                                }
+                            if (currentHour == schedule[jj].milTime )
+                                {
+                                    document.getElementById(_times[jj]).style.background = "#83677B";
+                                }
+                            if(currentHour < schedule[jj].milTime)
+                                {
+                                    document.getElementById(_times[jj]).style.background = "#ADADAD";
+                                }
+                                    
+                        }
+                    }
+            }
